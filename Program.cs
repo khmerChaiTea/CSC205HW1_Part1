c79898d7b98f7f79fd8b7f6f6cd6b5819b0b5a7a
pchai <prempreychhay@gmail.com>
2024-07-05 08:37:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Displaying name
+            Console.WriteLine("My is Prem Chhay.");
+
+            // Displaying interesting facts
+            Console.WriteLine("Here are a few interesting facts:");
+            Console.WriteLine("- Aspiring Software Developer.");
+            Console.WriteLine("- I am a transitioning service member of 20 years in the US Navy with an active Secret clearance.");
+            Console.WriteLine("- Fluent in both English and Khmer, I bring a global perspective to my work.");
+
+            // Keep console window open until a key is pressed
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
